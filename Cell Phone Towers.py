534b7c23a48a479ab07cc663e6e58e8041a80542
ALBERT WILLEMSE <38205742@student365.msfed.nwu.ac.za>
2023-11-26 00:20:50 +0200

    Refactor code

--- Cell Phone Towers.py
+++ Cell Phone Towers.py
@@ -2,173 +2,156 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-# Given data
-# cell_data = [
-#     ("A", 536660, 183800, -0.03098, 51.53657),
-#     ("B", 537032, 184006, -0.02554, 51.53833),
-#     ("C", 537109, 183884, -0.02448, 51.53721),
-#     ("D", 537110, 184695, -0.02415, 51.5445),
-#     ("E", 537206, 184685, -0.02277, 51.54439),
-#     ("F", 537248, 185016, -0.02204, 51.54735),
-#     ("G", 537250, 185020, -0.02201, 51.54739),
-#     ("H", 537267, 184783, -0.02185, 51.54525),
-#     ("I", 537269, 183451, -0.02234, 51.53328),
-#     ("J", 537270, 184140, -0.02206, 51.53948),
-#     ("K", 537356, 184927, -0.02052, 51.54653),
-#     ("L", 537380, 184727, -0.02025, 51.54472),
-#     ("M", 537458, 184495, -0.01921, 51.54262),
-#     ("N", 537604, 184134, -0.01725, 51.53934),
-#     ("O", 537720, 184057, -0.01561, 51.53862),
-#     ("P", 537905, 184591, -0.01273, 51.54337),
-#     ("Q", 537910, 184441, -0.01272, 51.54202),
-#     ("R", 537953, 184295, -0.01216, 51.5407),
-#     ("S", 538050, 184245, -0.01078, 51.54023),
-# ]
+# Set plot titles and labels for Eastings and Northings plot
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
 
-def calculate_distance(cell1, cell2):
-    distance = math.sqrt((cell1[1] - cell2[1])**2 + (cell1[2] - cell2[2])**2)
-    # print( str(cell1[1]) + " " + str(cell2[2]) + " : " + str(distance))
+#region Data Processing Functions
+
+# Function to calculate Euclidean distance between two cells
+def calculate_distance(cell1, cell2,i,j):
+    distance = math.sqrt((cell1[i] - cell2[i])**2 + (cell1[j] - cell2[j])**2)
     return distance
 
+# Function to find the cell closest to the centroid of all cells
 def find_center_cell(cells):
-    # Extract coordinates into NumPy array for efficient calculations
     coordinates = np.array([cell[1:3] for cell in cells])
-
-    # Calculate the centroid
     centroid = np.mean(coordinates, axis=0)
-
-    # Find the cell closest to the centroid
     center_cell = min(cells, key=lambda x: np.linalg.norm(np.array(x[1:3]) - centroid))
-
     return center_cell
 
-def assign_frequencies(cells):
-    # center_cells = sorted(cells, key=lambda x: sum(calculate_distance(x, other) for other in cells))
-    # print("Center Node:", center_cells[0])
-
+# Function to assign frequencies to cells based on their proximity
+def assign_frequencies(cells, index_1, index_2):
+    # Find the center cell
     center_cell = find_center_cell(cells)
-    print("Center Cell:", center_cell)
     
-
-    sorted_cells = sorted(cells, key=lambda x: calculate_distance(center_cell, x))
+    # Sort cells based on distance from the center cell
+    sorted_cells = sorted(cells, key=lambda x: calculate_distance(center_cell, x, index_1, index_2))
     
-
-
-    # Set the first 6 frequencies (110 - 115) to the center cell and the closest cells
+    # Assign the first 6 frequencies (110 - 115) to the center cell and its closest neighbors
     for i in range(6):
         sorted_cells[i][5] = 110 + i
 
-    print("Sorted Cells:", sorted_cells)
-    print("Sorted Cell 6:", sorted_cells[6])
-
     # Assign frequencies to the remaining cells based on distance from the furthest away frequency
     for current_cell in sorted_cells[6:]:
-        
         furthest_away_distance = 0
         furthest_away_frequency = None
-        
-        neighbor_cells = sorted(sorted_cells, key=lambda x: calculate_distance(current_cell, x))
-        print("Neighbor Cells: " + str(neighbor_cells))
-        print("current_cell: " + str(current_cell[0]))
+        neighbor_cells = sorted(sorted_cells, key=lambda x: calculate_distance(current_cell, x, index_1, index_2))
 
-        # Track the frequencies that have been used
+        # Track the frequencies that have been used closest to the current cell
         used_frequencies = set(range(110, 116))
 
         for neighbor_cell in neighbor_cells:
-            
             if neighbor_cell[5] != -1:
-                
                 if used_frequencies:
-                    distance = calculate_distance(current_cell, neighbor_cell)
-
-                    print("used_frequencies : " + str(used_frequencies))
-                    # Mark the frequency as used
+                    distance = calculate_distance(current_cell, neighbor_cell, index_1, index_2)
+                    # Mark the frequency as used until furthest frequency is found
                     used_frequencies.discard(neighbor_cell[5])
-                    
 
                     # Update furthest away frequency if the distance is greater
                     if distance > furthest_away_distance:
                         furthest_away_distance = distance
                         furthest_away_frequency = neighbor_cell[5]
-                        
-            
-            
-                        
-        print("furthest_away_frequency : " + str(furthest_away_frequency))
 
         # Assign the furthest away frequency to the current cell
         current_cell[5] = furthest_away_frequency
 
-        
     return sorted_cells
 
+#endregion
 
 
+#region Data Loading
 
+# Load cell data from a user-selected file
 cell_data = []
+file_choice = input("given-data-1.txt \nsample-data-2.txt \nsample-data-3.txt \nChoose a file: (type 1, 2, or 3): ")
 
-with open('sample-data.txt') as file:
+if file_choice == '1':
+    file_name = "given-data-1.txt"
+elif file_choice == '2':
+    file_name = "sample-data-2.txt"
+elif file_choice == '3':
+    file_name = "sample-data-3.txt"
+else:
+    print("Using default script")
+    file_name = "given-data-1.txt"
+
+with open(file_name) as file:
     for line in file:
         row = line.strip().split(',')
         cell_ids, eastings, northings, longitudes, latitudes, frequency = [i.strip() for i in row]
-
-        # Store the data as separate lists
         cell_data.append([cell_ids, int(eastings), int(northings), float(longitudes), float(latitudes), int(frequency)])
 
+coordinates_choice = input("Choose a coordinate's system: (type 1 (Easting and northing) or 2 (Latitude and longitude)): ")
 
-# Frequency allocation
-cell_data = assign_frequencies(cell_data)
+# Assign frequencies to cells
+if coordinates_choice == '1':  
+    cell_data = assign_frequencies(cell_data, 1, 2)
+elif coordinates_choice == '2':
+    cell_data = assign_frequencies(cell_data, 3, 4)
+else:
+    print("Using default (Easting and northing)")
+    cell_data = assign_frequencies(cell_data, 1, 2)
 
 # Extracting data into separate lists
 cell_ids, eastings, northings, longitudes, latitudes, frequency = zip(*cell_data)
 
-
-# Converting lists to NumPy arrays
-xpoints = np.array(eastings)
-ypoints = np.array(northings)
+# Converting lists to NumPy arrays for plotting
+eastings = np.array(eastings)
+northings = np.array(northings)
 longitudes = np.array(longitudes)
 latitudes = np.array(latitudes)
 
+#endregion
 
+#region Printing results
 
+# Print the results at the end
+print("\nAllocated Frequencies:")
 
-# Plotting the scatter plot and assigning it to 'scatter'
-scatter = plt.scatter(xpoints, ypoints, c=frequency, s=1700, cmap='gist_rainbow', edgecolors='red', alpha=0.6)
+# Sort cell_data alphabetically based on cell IDs
+sorted_cell_data = sorted(cell_data, key=lambda x: x[0])
 
-# Add a colorbar
-cbar = plt.colorbar(scatter, label='Frequencies')
+# Print only cell ID and allocated frequency
+for cell in sorted_cell_data:
+    print(f"{cell[0]} | {cell[5]}")
 
-# Label points with eastings & northings
-count = 0
-for (i, j) in zip(xpoints, ypoints):
-    plt.text(i, j, (cell_ids[count] ), va='center', ha='center')
-    count += 1
+#endregion
 
-font1 = {'family':'serif','color':'blue','size':20}
-font2 = {'family':'serif','color':'darkred','size':15}
+#region Plotting Functions
 
-plt.title("Cell Phone Towers", fontdict = font1)
-plt.xlabel("Eastings", fontdict = font2)
-plt.ylabel("Northings", fontdict = font2)
+# Assign frequencies to cells
+if coordinates_choice == '2':
+    # Plotting the scatter plot for Longitudes and Latitude
+    scatter = plt.scatter(longitudes, latitudes, c=frequency, s=1700, cmap='gist_rainbow', edgecolors='red', alpha=0.6)
+    cbar = plt.colorbar(scatter, label='Frequencies')
 
-plt.show()
+    # Label points with cell IDs for Longitudes and Latitudes plot
+    count = 0
+    for (i, j) in zip(longitudes, latitudes):
+        plt.text(i, j, (cell_ids[count] ), va='center', ha='center')
+        count += 1
 
-# Label points with longitudes & latitudes
+    # Set plot titles and labels for Longitudes and Latitudes plot
+    plt.title("Cell Phone Towers - Longitudes and Latitudes", fontdict=font1)
+    plt.xlabel("Longitudes", fontdict=font2)
+    plt.ylabel("Latitudes", fontdict=font2)
+    plt.show()
+else:
+    # Plotting the scatter plot for Eastings and Northings
+    scatter = plt.scatter(eastings, northings, c=frequency, s=1700, cmap='gist_rainbow', edgecolors='red', alpha=0.6)
+    cbar = plt.colorbar(scatter, label='Frequencies')
 
-count = 0
-for (i, j) in zip(longitudes, latitudes):
-    plt.text(i, j, (cell_ids[count]), va='center', ha='center')
-    count += 1
+    # Label points with cell IDs for Eastings and Northings plot
+    count = 0
+    for (i, j) in zip(eastings, northings):
+        plt.text(i, j, (cell_ids[count] ), va='center', ha='center')
+        count += 1
 
+    plt.title("Cell Phone Towers - Eastings and Northings", fontdict=font1)
+    plt.xlabel("Eastings", fontdict=font2)
+    plt.ylabel("Northings", fontdict=font2)
+    plt.show()
 
-plt.scatter(longitudes, latitudes, s= 1500, edgecolors = 'red',  alpha = 0.3)
-
-font1 = {'family':'serif','color':'blue','size':20}
-font2 = {'family':'serif','color':'darkred','size':15}
-
-plt.title("Cell Phone Towers", fontdict = font1)
-plt.xlabel("Longitudes", fontdict = font2)
-plt.ylabel("Latitudes", fontdict = font2)
-
-plt.show()
-
+#endregion
